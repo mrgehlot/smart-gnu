79a3438b273b01fc8017e9e04a21030bad495692
PeaceInSpace <abhigelot123@gmail.com>
2020-02-13 02:34:03 +0530

    first-commit

--- smartGNU/smartGNU/settings.py
+++ smartGNU/smartGNU/settings.py
@@ -144,11 +144,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "smartGNU/smart_gnu/static")
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "smartGNU/static"),
-# )
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 GOOGLE_CLIENT_ID = '85906610984-qog0si0va6si6tnj3tij46b7r3gv9n8g.apps.googleusercontent.com'
 
