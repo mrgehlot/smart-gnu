f9cef63aa1acd23143850c9ce666882b2f189c49
PeaceInSpace <abhigelot123@gmail.com>
2020-02-13 02:24:16 +0530

    first-commit

--- smartGNU/smartGNU/settings.py
+++ smartGNU/smartGNU/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "smartGNU/smart_gnu/static")
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "smartGNU/static"),
 # )
