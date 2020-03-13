8ff935dfd893a5260c5282a8ffda0f77eb3bb420
PeaceInSpace <abhigelot123@gmail.com>
2020-03-14 03:00:58 +0530

    migrations

--- smartGNU/smart_gnu/signals.py
+++ smartGNU/smart_gnu/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import pre_save, post_save
 from .models import Device
 from .mqtt_code import request_for_publish
-from .models import GPIO_PINS,NodeMCU
+from .models import NodeMCU
 from django.db import transaction
 def add_topic(instance, **kwargs):
     instance.topic = instance.node_mcu.node_mcu_ip
@@ -11,12 +11,13 @@ pre_save.connect(add_topic,sender=Device)
 #     if created:
 #         request_for_publish(topic=instance.lab.lab_number.replace(" ",""),pay_load=instance.device_name.replace(" ",""))
 # post_save.connect(publish_topic,sender=Device)
+GPIO_PINS = [0,1,2,3,4,5,12,13,14,15,16]
 
 def add_devices(instance,created,**kwargs):
     with transaction.atomic():
         if created:
-            for i in range(1,len(GPIO_PINS)+1):
-                Device.objects.get_or_create(device_name ="{}-{}".format(instance.lab.lab_number,i),
+            [Device.objects.get_or_create(device_name ="{}-{}".format(instance.lab.lab_number,i),
                                       topic = instance.node_mcu_ip, gpio_pin = i,
-                                      node_mcu = instance)
+                                      node_mcu = instance) for i in GPIO_PINS]
+
 post_save.connect(add_devices,sender=NodeMCU)
